66ef2c1e87089c3169b386054aea437005c41446
Ben Buzard <250144benjamin@gmail.com>
2022-07-14 17:09:23 +0000

    Commit many changes

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Computer Vision Web Scaffold
+
 A scaffold for deploying dockerized flask applications.
 
 If you have any questions, feel free to open an issue on [Github](https://github.com/organization-x/omni/issues).
 
 ### Video Guide
+
 [![Deploy a Web Project with Flask](https://img.youtube.com/vi/JUb-PpejA7w/0.jpg)](https://youtu.be/JUb-PpejA7w "Deploy a Web Project with Flask")
 
 This guide covers how you can quickly deploy most projects with the [Flask](https://flask.palletsprojects.com/) framework and our omni scaffold.
@@ -68,6 +70,7 @@ to fix this run
 `sed -i 's/\r$//' entrypoint.sh`
 
 ### File Structure
+
 The files/directories which you will need to edit are **bolded**
 
 **DO NOT TOUCH OTHER FILES. THIS MAY RESULT IN YOUR PROJECT BEING UNABLE TO RUN**
@@ -88,13 +91,20 @@ The files/directories which you will need to edit are **bolded**
           - **index.html**
 
 ### How to upload best.pt to your file structure?
+
 Run 
 `cp ../path/to/best.pt best.pt`
+
 ### best.pt ###
+
 The weights file - must upload if you are running file on coding center or are trying to deploy.
+
 ### main.py ###
+
 Contains the main flask app itself.
+
 ### requirements.txt ###
+
 Contains list of packages and modules required to run the flask app. Edit only if you are using additional packages that need to be pip installed in order to run the project.
 
 To generate a requirements.txt file you can run
@@ -102,16 +112,25 @@ To generate a requirements.txt file you can run
 `pip list --format=freeze > app/requirements.txt`
 
 the requirements.txt file will then be updated. Keep in mind: some packages you install on one operating system may not be available on another. You will have to debug and resolve this yourself if this is the case.
+
 ### static/ ###
+
 Contains the static images, CSS, & JS files used by the flask app for the webpage. You will need to create this and put files in it. Place all your images used for your website in static/images/ so that you can then reference them in your html files.
+
 ### utils.py ###
+
 Contains common functions used by the flask app. Put things here that are used more than once in the flask app.
+
 ### templates/ ###
+
 Contains the HTML pages used for the webpage. Edit these to fit your project. index.html is the demo page.
+
 ### Files used for deployment ###
+
 `config.py`
 `Dockerfile`
 `entrypoint.sh`
 `nginx_host`
 `host_config`
 **Only modify `host_config`. Do not touch the other files.**
+
--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 from flask import send_from_directory
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, flash, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
 from flask import render_template
 from url_utils import get_base_url
 import os
 import torch
+import json
+import urllib.parse
+import cv2
 
 # setup the webserver
 # port may need to be changed if there are multiple flask servers running on same server
-port = 12345
+port = 25563
 base_url = get_base_url(port)
 
 # if the base url is not empty, then the server is running in development, and we need to specify the static folder so that the static files are served
@@ -22,12 +25,58 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-model = torch.hub.load("ultralytics/yolov5", "custom", path = 'best.pt', force_reload=True)
-
+model_id = torch.hub.load("ultralytics/yolov5", "custom", path = 'ident.pt', force_reload=True)
+model_toma = torch.hub.load("ultralytics/yolov5", "custom", path = 'toma.pt', force_reload=True)
+model_grape = torch.hub.load("ultralytics/yolov5", "custom", path = 'grape.pt', force_reload=True)
+model_apple = torch.hub.load("ultralytics/yolov5", "custom", path = 'apple.pt', force_reload=True)
+model_blue = torch.hub.load("ultralytics/yolov5", "custom", path = 'blueberry.pt', force_reload=True)
+# model_aloe =  torch.hub.load("ultralytics/yolov5", "custom", path = 'aloe.pt', force_reload=True)
+model_snake = torch.hub.load("ultralytics/yolov5", "custom", path = 'snake.pt', force_reload=True)
+model_orange = torch.hub.load("ultralytics/yolov5", "custom", path = 'orange.pt', force_reload=True)
+# model_squash = torch.hub.load("ultralytics/yolov5", "custom", path = 'squash.pt', force_reload=True)
+model_mango = torch.hub.load("ultralytics/yolov5", "custom", path = 'mango.pt', force_reload=True)
+model_stra_rasp =  torch.hub.load("ultralytics/yolov5", "custom", path = 'straw_rasp.pt', force_reload=True)
+model_pepper =  torch.hub.load("ultralytics/yolov5", "custom", path = 'pepper.pt', force_reload=True)
+model_peach = torch.hub.load("ultralytics/yolov5", "custom", path = 'peach.pt', force_reload=True)
+# model_cherry = torch.hub.load("ultralytics/yolov5", "custom", path = 'cherry.pt', force_reload=True)
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_model(step, detected_plant):
+    if step == 'plant':
+        return model_id
+    
+    if detected_plant == "Tomato plant":
+        return model_toma
+    elif detected_plant == "Grape plant":
+        return model_grape
+    elif detected_plant == "Apple plant":
+        return model_apple
+    elif detected_plant == "Blueberry plant":
+        return model_blue
+    # elif detected_plant == "Aloe Vera":
+    #     return model_aloe
+    elif detected_plant == "Snake plant":
+        return model_snake
+    elif detected_plant == "Orange plant":
+          return model_orange
+    # elif detected_plant == "Squash plant":
+    #     return model_squash
+    elif detected_plant == "Mango plant":
+        return model_mango
+    elif detected_plant == "Strawberry plant" or "Raspberry plant":
+        return model_stra_rasp
+    elif detected_plant == "Pepper plant":
+        return model_pepper
+    elif detected_plant == "Peach plant":
+        return model_peach
+    # elif detected_plant == "Cherry plant":
+    #     return model_cherry
+    else:
+        raise NotImplementedError(f"Unknown plant type: {detected_plant}")
 
 @app.route(f'{base_url}', methods=['GET', 'POST'])
 def home():
@@ -53,15 +102,82 @@ def home():
     return render_template('home.html')
 
 
+@app.route(f'{base_url}/use-model', methods=['GET', 'POST'])
+def use_model():
+    step = request.args.get('step')
+    detected_plant = request.args.get('detected_plant')
+    plant_confidence = request.args.get('plant_confidence')
+    detected_disease = request.args.get('detected_disease')
+    disease_confidence = request.args.get('disease_confidence')
+    plant_filename = request.args.get('plant_filename')
+    disease_filename = request.args.get('disease_filename')
+
+    if request.method == 'POST':
+        step = request.form['step']
+        detected_plant = request.form['detected_plant']
+        plant_confidence = request.form['plant_confidence']
+        detected_disease = request.form['detected_disease']
+        disease_confidence = request.form['disease_confidence']
+        plant_filename = request.form['plant_filename']
+        disease_filename = request.form['disease_filename']
+        url = request.url.replace(("&" if "?" in request.url else "?") + "error=FILE_EXT_NOT_VALID", "")
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(url)
+        
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(url)
+
+        if not allowed_file(file.filename):
+            return redirect(url + ("&" if "?" in request.url else "?") + "error=FILE_EXT_NOT_VALID")
+
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename) + f"?step={step}&detected_plant={detected_plant}&plant_confidence={plant_confidence}&detected_disease={detected_disease}&disease_confidence={disease_confidence}&plant_filename={plant_filename}&disease_filename={disease_filename}")
+
+    if 'error' in request.args:
+        error = request.args.get('error')
+    else:
+        error = 'NONE'
+
+    if 'filename' in request.args:
+        filename = request.args.get('filename')
+    else:
+        filename = ''
+    
+    return render_template('use_model.html', error=error, filename=filename, step=step, detected_plant=detected_plant, plant_confidence=plant_confidence, detected_disease=detected_disease, disease_confidence=disease_confidence, plant_filename=plant_filename, disease_filename=disease_filename)
+
+
 @app.route(f'{base_url}/uploads/<filename>')
 def uploaded_file(filename):
+    step = request.args.get('step')
+    detected_plant = request.args.get('detected_plant')
+    plant_confidence = request.args.get('plant_confidence')
+    detected_disease = request.args.get('detected_disease')
+    disease_confidence = request.args.get('disease_confidence')
+    plant_filename = request.args.get('plant_filename')
+    disease_filename = request.args.get('disease_filename')
+
     here = os.getcwd()
     image_path = os.path.join(here, app.config['UPLOAD_FOLDER'], filename)
+    model = get_model(step, detected_plant)
     results = model(image_path, size=416)
     if len(results.pandas().xyxy) > 0:
         results.print()
         save_dir = os.path.join(here, app.config['UPLOAD_FOLDER'])
         results.save(save_dir=save_dir)
+        filename = filename.replace(filename.split('.')[-1], filename.split('.')[-1].lower())
+        image_path = os.path.join(here, app.config['UPLOAD_FOLDER'], filename)
+
+        # img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+        # resized = cv2.resize(img, (256, 256))
+        # cv2.imwrite(image_path, resized)
+
         def and_syntax(alist):
             if len(alist) == 1:
                 alist = "".join(alist)
@@ -87,12 +203,28 @@ def uploaded_file(filename):
         labels = set(labels)
         labels = [emotion.capitalize() for emotion in labels]
         labels = and_syntax(labels)
-        return render_template('results.html', confidences=format_confidences, labels=labels,
-                               old_filename=filename,
-                               filename=filename)
+
+        print(labels)
+        print(format_confidences)
+        
+        if step == 'plant':
+            step = 'disease'
+            detected_plant = labels
+            plant_confidence = format_confidences
+            plant_filename = filename
+        elif step == 'disease':
+            step = 'finish'
+            detected_disease = labels
+            disease_confidence = format_confidences
+            disease_filename = filename
+        else:
+            print("person somehow broke this")
+
+        return redirect(url_for('use_model', plant_filename=plant_filename, disease_filename=disease_filename, step=step, detected_plant=detected_plant, plant_confidence=plant_confidence, detected_disease=detected_disease, disease_confidence=disease_confidence))
+        
     else:
         found = False
-        return render_template('results.html', labels='No Emotion', old_filename=filename, filename=filename)
+        return redirect(url_for('use_model', error="NOT_FOUND", filename=filename, step=step, detected_plant=detected_plant, plant_confidence=plant_confidence, detected_disease=detected_disease, disease_confidence=disease_confidence))
 
 
 @app.route(f'{base_url}/uploads/<path:filename>')
@@ -107,7 +239,7 @@ def files(filename):
 
 if __name__ == '__main__':
     # IMPORTANT: change url to the site where you are editing this file.
-    website_url = 'url'
+    website_url = 'https://cocalc15.ai-camp.dev'
     
-    print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
+    print(f'Try to open\n\n    {website_url}' + base_url + '\n\n')
     app.run(host = '0.0.0.0', port=port, debug=True)
